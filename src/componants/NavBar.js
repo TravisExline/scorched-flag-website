54f300b45cc3506bb9e0e476aa458d138c28c296
TravisExline <t.exline22@gmail.com>
2020-06-10 09:28:10 -0500

    permissions issues?

--- src/componants/NavBar.js
+++ src/componants/NavBar.js
@@ -4,12 +4,16 @@ import {NavLink} from 'react-router-dom'
 const NavBar = () => 
     <div className="nav-links">
         <NavLink className="about-me-link"
-            to='/'
+            to='/about-me'
             exact>About SWF Co.
         </NavLink>
         <NavLink className="flags-link"
-            to='/'
+            to='/flags'
             exact>Homemade Flags
+        </NavLink>
+        <NavLink className="about-flags-link"
+            to='/about-flags'
+            exact>About Our Flags
         </NavLink>
     </div>
 
